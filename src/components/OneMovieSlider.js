5078c83fd52a8df2712271ac777a8834e287a787
Tomixer <tomix.klokar@gmail.com>
2023-10-11 23:12:04 +0200

    pridal som funkcie na tlacitka

--- src/components/OneMovieSlider.js
+++ src/components/OneMovieSlider.js
@@ -4,9 +4,43 @@ import {useState} from "react"
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 const OneMovieSlider=()=>{
     const [index,setIndex]=useState(0)
-    const {}=data[0]
-    return <div>
+    const {image,title,description}=data[index]
 
+    const checkMovieNumber=(movieIndex)=>{
+        if(movieIndex<0){
+            return data.length-1
+        }else if(movieIndex>data.length-1){
+            return 0
+        }else{
+            return movieIndex
+        }
+
+        
+    }   
+
+    const nextMovie=()=>{
+        setIndex((index)=>{
+            const newIndex=index+1
+            return checkMovieNumber(newIndex)
+        })
+    }
+    const previousMovie=()=>{
+        setIndex((index)=>{
+            const newIndex=index-1
+            return checkMovieNumber(newIndex)
+        })
+    }
+
+    return <div>
+        <img src={image} alt="" />
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <button onClick={previousMovie}>
+        <FaArrowAltCircleLeft />
+        </button>
+        <button onClick={nextMovie}>
+        <FaArrowAltCircleRight />
+        </button>
     </div>
 }
 export default OneMovieSlider
